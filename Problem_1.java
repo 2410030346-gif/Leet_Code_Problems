0c450326050a78fce88a5ca24f6282615948a5a8
Niranjan <2410030346@klh.edu.in>
2026-01-04 20:04:42 +0530

    add two sum java solution

---
+++ Problem_1.java
@@ -0,0 +1,28 @@
+class Problem_1 {
+    public int[] twoSum(int[] nums, int target) {
+        int [] arr=new int[2];
+        for (int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                  arr[0]=i;
+                  arr[1]=j;
+                  break;
+                }
+            }
+        }
+        return arr;
+        
+    }
+
+ public static void main(String[] args) {
+        Problem_1 obj = new Problem_1();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] res = obj.twoSum(nums, target);
+        System.out.println(res[0] + " " + res[1]);
+    }
+}
