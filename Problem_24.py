c32cc9f0d4d6671d1a25f621bf3af0a666617c2f
Niranjan <2410030346@klh.edu.in>
2026-01-26 19:47:33 +0530

    swap pairs

---
+++ Problem_24.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from Problem_19 import ListNode
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        while head and head.next:
+            first = head
+            second = head.next
+            prev.next = second
+            first.next = second.next
+            second.next = first
+            prev = first
+            head = first.next
+        return dummy.next
