2404b89f9af0e1760c480da6062112334f7add1c
Niranjan <2410030346@klh.edu.in>
2026-03-11 20:29:08 +0530

    insert

--- Problem_57.py
+++ Problem_57.py
@@ -1,26 +1,21 @@
-class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-        int i = 0;
-        int n = intervals.length;
+class Solution:
+    def insert(self, intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+        res = []
+        i = 0
+        n = len(intervals)
 
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            res.add(intervals[i]);
-            i++;
-        }
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
 
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-            i++;
-        }
-        res.add(newInterval);
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        res.append(newInterval)
 
-        while (i < n) {
-            res.add(intervals[i]);
-            i++;
-        }
+        while i < n:
+            res.append(intervals[i])
+            i += 1
 
-        return res.toArray(new int[res.size()][]);
-    }
-}
+        return res
